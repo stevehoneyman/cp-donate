1125416b15ff62a72af807184449e6d34a1ed127
Steve Honeyman <hachimitsu@Steves-MacBook-Pro-2.local>
2021-08-24 12:02:34 +0100

    opening statment removed, info example added

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
 // Check js is running
 var dateTime = new Date();
 console.log('loading from scripts.js in cp-donate on : ' + dateTime);
+
+$(document).ready(function() {
+  $('.amount li').on('click', function() {
+     $(this).next().toggleClass('show-data')
+  }); // end on click
+}); // close doc ready
