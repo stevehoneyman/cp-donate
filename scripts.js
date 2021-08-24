4e816b7707ad2f6b3737745a67146d00f762bf4f
Steve Honeyman <hachimitsu@Steves-MacBook-Pro-2.local>
2021-08-24 18:37:58 +0100

    additional [marketing] fields added to payment pages, pages re-structured

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ console.log('loading from scripts.js in cp-donate on : ' + dateTime);
 
 $(document).ready(function() {
   $('.amount li').on('click', function() {
-     $(this).next().toggleClass('show-data')
+     $(this).next().toggleClass('show-data');
+     // console.log('clicked');
   }); // end on click
 }); // close doc ready
